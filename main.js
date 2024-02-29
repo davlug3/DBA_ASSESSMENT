7762704eac21d232d91cd44435a9f23c3813fe79
davlug3 <davelugtu1@gmail.com>
2024-03-01 00:54:43 +0800

    number 3 answered

--- main.js
+++ main.js
@@ -34,11 +34,85 @@ const printNumberPattern = () => {
 
   return outputY
 }
+``
+console.log(printNumberPattern())
 
 
 
 
+//3
+class Employees {
+  constructor(name) {
+    let count = {
+      male: 0, 
+      female: 0,
+    }
+    this.name = name ?? ''
 
 
-console.log(
-printNumberPattern())
+    this.getCount = (gender) => {
+      if (gender === 'male' || gender === 'female') {
+        return count[gender]
+      }
+
+      if (!gender) {
+        let out = 0
+        Object.values(count).forEach(n => out = out + Number(n) )
+        return out
+      }
+    }
+
+    this.setNumber = (gender, number) => {
+      if (gender === 'male' || gender === 'female') {
+        count[gender] = number ?? prompt(`Enter the number of ${this.name} ${gender}s:`)
+      }
+    };
+
+  }
+
+}
+
+
+
+const processEmployees = () => {
+  const newly_hired = new Employees("newly hired")
+  newly_hired.setNumber("male")
+  newly_hired.setNumber("female")
+
+  const permanent_position = new Employees("permanent position")
+  permanent_position.setNumber("male")
+  permanent_position.setNumber("female")
+
+  const resigned = new Employees("resigned")
+  resigned.setNumber("male")
+  resigned.setNumber("female")
+  
+
+
+  console.log("===")
+  console.log("Thank you for the information")
+  console.log("===")
+  console.log("Here is the summary !!!")
+
+
+  console.log("Number of hired employee:" ,
+    newly_hired.getCount(),
+    newly_hired.getCount("male"),
+    newly_hired.getCount("female")
+    )
+    
+  console.log("Number of Permanent Employee:" ,
+    permanent_position.getCount(),
+    permanent_position.getCount("male"),
+    permanent_position.getCount("female"),
+    )
+
+  console.log("Number of Resigned Employee:" ,
+    resigned.getCount(),
+    resigned.getCount("male"),
+    resigned.getCount("female")
+    )
+
+}
+
+processEmployees()
